49f51b581b70e1b1aab93cb7c0d70be22c878721
洋佑 堀川 <uplifting0760@gmail.com>
2015-10-26 00:51:17 +0900

    文字列と数字の出力をするクラスを作成

---
+++ src/number1/Practice1.java
@@ -0,0 +1,9 @@
+package number1;
+
+public class Practice1 {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println(100);
+		System.out.println("数字はダブルクォートなしで出力出来ます。");
+	}
+}
