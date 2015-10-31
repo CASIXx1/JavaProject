66630f78848f4530a660e7b4f7f4bf280bfe2bac
洋佑 堀川 <uplifting0760@gmail.com>
2015-11-01 02:57:13 +0900

    不必要な変数を削除

--- src/mathmatics/Calculator.java
+++ src/mathmatics/Calculator.java
@@ -1,33 +1,27 @@
 package mathmatics;
 
-import java.util.Scanner;
-
 public class Calculator {
-		private int num1,num2,ans;
+		private int ans;
 		
 		public int add(int num1, int num2){ 
-			Scanner sc = new Scanner(System.in);
 			ans = num1 + num2;
 			System.out.println("X + Y = " + ans);
 			return ans;
 		}
 		
 		public int subtract(int num1, int num2){ 
-			Scanner sc = new Scanner(System.in);
 			ans = num1 - num2;
 			System.out.println("X - Y = " + ans);
 			return ans;
 		}
 		
 		public int multiplication(int num1, int num2){ 
-			Scanner sc = new Scanner(System.in);
 			ans = num1 * num2;
 			System.out.println("X * Y = " + ans);
 			return ans;
 		}
 		
 		public int division(int num1, int num2){ 
-			Scanner sc = new Scanner(System.in);
 			ans = num1 / num2;
 			System.out.println("X / Y = " + ans);
 			return ans;
