a7878141eed504217512bb74f9eb798057e1ace0
洋佑 堀川 <uplifting0760@gmail.com>
2015-10-31 23:58:52 +0900

    CalculatorMain実行時に、addクラスかsubtractクラスを選べるように修正

--- src/mathmatics/Calculator.java
+++ src/mathmatics/Calculator.java
@@ -5,23 +5,17 @@ import java.util.Scanner;
 public class Calculator {
 		private int num1,num2,ans;
 		
-		public void add(){ 
-		Scanner sc = new Scanner(System.in);
-		System.out.print("x:");
-		num1 = sc.nextInt();
-		System.out.print("y:");
-		num2 = sc.nextInt();
-		ans = num1 + num2;
-		System.out.println("x + y = " + ans);
+		public int add(int num1, int num2){ 
+			Scanner sc = new Scanner(System.in);
+			ans = num1 + num2;
+			System.out.println("X + Y = " + ans);
+			return ans;
 		}
 		
-		public void subtract(){ 
+		public int subtract(int num1, int num2){ 
 			Scanner sc = new Scanner(System.in);
-			System.out.print("x:");
-			num1 = sc.nextInt();
-			System.out.print("y:");
-			num2 = sc.nextInt();
 			ans = num1 - num2;
-			System.out.println("x - y = " + ans);
-			}
+			System.out.println("X - Y = " + ans);
+			return ans;
+		}
 }
--- src/mathmatics/CalculatorMain.java
+++ src/mathmatics/CalculatorMain.java
@@ -1,9 +1,29 @@
 package mathmatics;
 
+import java.util.Scanner;
+
 public class CalculatorMain {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		Calculator calTest = new Calculator();
-		calTest.add();
-		calTest.subtract();
+		
+		int num1, num2, calcType;
+		System.out.print("計算の種類は何ですか？");
+		calcType = sc.nextInt();
+		
+		System.out.print("X : ");
+		num1 = sc.nextInt();
+		System.out.print("Y : ");
+		num2 = sc.nextInt();
+		
+		switch (calcType) {
+		case 1:
+			calTest.add(num1, num2);
+			break;
+
+		default:
+			calTest.subtract(num1, num2);
+			break;
+		}
 	}
 }
