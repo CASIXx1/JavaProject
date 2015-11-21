819d0801fe0bdcb215c20ef51da65fc26c8b2459
洋佑 堀川 <uplifting0760@gmail.com>
2015-11-22 02:15:40 +0900

    目的外の処理をするクラスを削除

--- src/naturaLanguageProcessing/Practice1.java
+++
@@ -1,9 +0,0 @@
-package naturaLanguageProcessing;
-
-public class Practice1 {
-	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println(100);
-		System.out.println("数字はダブルクォートなしで出力出来ます。");
-	}
-}
